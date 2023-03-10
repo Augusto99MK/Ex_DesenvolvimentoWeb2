5076dc137519f4b065afe946497777c47c7f162a
Augusto99MK <carlosaugustox6@gmail.com>
2023-03-10 13:01:50 -0400

    Atividade2_

--- atividade2/index.php
+++ atividade2/index.php
@@ -7,21 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./style.css">
+    <script>
+        function mudarCorInput() {
+            alert("Seu background mudou");
+            document.getElementById("nomeID").style.background = '#87CEFA';
+        }
+
+        function procurarCidade() {
+            document.getElementById("formCadastroDeAluno").submit();
+        }
+    </script>
 </head>
 
 <body>
 
     <input type="text" onclick="mudarCorInput()" placeholder="Mude a Cor" name="name" id="nomeID">
-    <script>
-    function mudarCorInput() {
-        alert("Seu background mudou");
-        document.getElementById("nomeID").style.background = '#87CEFA';
-    }
 
-    function procurarCidade() {
-        document.getElementById("formCadastroDeAluno").submit();
-    }
-    </script>
     <div>
 
         <h1>Digite os seus filmes favoritos</h1>
@@ -47,28 +48,29 @@
         <div><input type="text" placeholder="Preferencia Cat. Aventura"></div>
         <div><input type="text" placeholder="Preferencia Cat. Comedia"></div>
     </div>
-
-    <select onchange="procurarCidade()" name="estado">
-        <option value="0" selected>Escolher</option>
-        <option value="sp">São Paulo</option>
-        <option value="ms">Mato Grosso do Sul</option>
-    </select>
-    <select id="cidadeID" name="cidade" class="form-control">
-        <?php
-        if ($_POST) {
-            if ($_POST["estado"] == "ms") {
-                echo "<option value=\"corumba\">Corumbá</option>";
-                echo "<option value=\"ladario\">Ladário</option>";
-            } else if ($_POST["estado"] == "sp") {
-                echo "<option value=\"andradina\">Andradina</option>";
-                echo "<option value=\"ilhaSolteira\">Ilha Solteira</option>";
-                echo "<option value=\"lorena\">Lorena</option>";
+    <form id="formCadastroDeAluno" method="POST" action="index.php">
+        <select id="estadoID" onchange="procurarCidade()" name="estado">
+            <option value="0" selected>Escolher</option>
+            <option value="sp">São Paulo</option>
+            <option value="ms">Mato Grosso do Sul</option>
+        </select>
+        <select id="cidadeID" name="cidade" class="form-control">
+            <?php
+            if ($_POST) {
+                if ($_POST["estado"] == "ms") {
+                    echo "<option value=\"corumba\">Corumbá</option>";
+                    echo "<option value=\"ladario\">Ladário</option>";
+                } else if ($_POST["estado"] == "sp") {
+                    echo "<option value=\"andradina\">Andradina</option>";
+                    echo "<option value=\"ilhaSolteira\">Ilha Solteira</option>";
+                    echo "<option value=\"lorena\">Lorena</option>";
+                }
+            } else {
+                echo "<option value=\"0\">Escolha o estado</option>";
             }
-        } else {
-            echo "<option value=\"0\">Escolha o estado</option>";
-        }
-        ?>
-    </select>
+            ?>
+        </select>
+    </form>
 </body>
 
 </html>
